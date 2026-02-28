9b23eaa8cb10a7fcc6a71b2e520612b26b33b9ad
Saksham <saksham4140@gmail.com>
2026-02-28 15:29:52 +0530

    Added time stamps

--- database.py
+++ database.py
@@ -21,13 +21,87 @@ items = [
     {'id':108, 'name':'pesticide', 'type':'input'}
 ]
 
+from datetime import datetime
+
 transit = [
-    {'batch_no': "BCH-ABC-001", 'user_from': 2, 'user_to': 1, 'item':102, 'qty':10},
-    {'batch_no': "BCH-ABC-002", 'user_from': 6, 'user_to': 3, 'item':107, 'qty':50},
-    {'batch_no': "BCH-ABC-003", 'user_from': 4, 'user_to': 5, 'item':100, 'qty':200},
-    {'batch_no': "BCH-ABC-004", 'user_from': 2, 'user_to': 7, 'item':101, 'qty':75},
-    {'batch_no': "BCH-ABC-005", 'user_from': 8, 'user_to': 1, 'item':103, 'qty':300},
-    {'batch_no': "BCH-ABC-006", 'user_from': 6, 'user_to': 4, 'item':105, 'qty':120}
+    {
+        'batch_no': "BCH-ABC-001",
+        'user_from': 2,
+        'user_to': 1,
+        'item': 102,
+        'qty': 10,
+        'status': "delivered",
+        'tracking_number': "TRK001",
+        'expected_delivery': "2026-03-01",
+        'placed_at': datetime(2026, 2, 20, 10, 30),
+        'dispatched_at': datetime(2026, 2, 21, 9, 0),
+        'delivered_at': datetime(2026, 2, 23, 14, 15)
+    },
+    {
+        'batch_no': "BCH-ABC-002",
+        'user_from': 6,
+        'user_to': 3,
+        'item': 107,
+        'qty': 50,
+        'status': "in_transit",
+        'tracking_number': "TRK002",
+        'expected_delivery': "2026-03-03",
+        'placed_at': datetime(2026, 2, 22, 11, 45),
+        'dispatched_at': datetime(2026, 2, 23, 8, 30),
+        'delivered_at': None
+    },
+    {
+        'batch_no': "BCH-ABC-003",
+        'user_from': 4,
+        'user_to': 5,
+        'item': 100,
+        'qty': 200,
+        'status': "packaging",
+        'tracking_number': None,
+        'expected_delivery': "2026-03-05",
+        'placed_at': datetime(2026, 2, 25, 16, 10),
+        'dispatched_at': None,
+        'delivered_at': None
+    },
+    {
+        'batch_no': "BCH-ABC-004",
+        'user_from': 2,
+        'user_to': 7,
+        'item': 101,
+        'qty': 75,
+        'status': "shipping",
+        'tracking_number': "TRK004",
+        'expected_delivery': "2026-03-04",
+        'placed_at': datetime(2026, 2, 24, 13, 0),
+        'dispatched_at': datetime(2026, 2, 25, 7, 45),
+        'delivered_at': None
+    },
+    {
+        'batch_no': "BCH-ABC-005",
+        'user_from': 8,
+        'user_to': 1,
+        'item': 103,
+        'qty': 300,
+        'status': "delivered",
+        'tracking_number': "TRK005",
+        'expected_delivery': "2026-03-02",
+        'placed_at': datetime(2026, 2, 19, 9, 20),
+        'dispatched_at': datetime(2026, 2, 20, 10, 0),
+        'delivered_at': datetime(2026, 2, 22, 18, 40)
+    },
+    {
+        'batch_no': "BCH-ABC-006",
+        'user_from': 6,
+        'user_to': 4,
+        'item': 105,
+        'qty': 120,
+        'status': "cancelled",
+        'tracking_number': None,
+        'expected_delivery': "2026-03-06",
+        'placed_at': datetime(2026, 2, 26, 14, 30),
+        'dispatched_at': None,
+        'delivered_at': None
+    }
 ]
 
 inventory = [
